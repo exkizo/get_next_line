b8be2db1f7dac05c010099c5e6c52aac69a3eb3e
Joao pedro Correia de oliveira pire magalhaes <jocorrei@c1r16s15.42lisboa.com>
2021-12-03 15:32:03 +0000

    latest version

---
+++ get_next_line.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jocorrei <jocorrei@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/18 15:52:23 by jocorrei          #+#    #+#             */
+/*   Updated: 2021/12/03 15:14:44 by jocorrei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+char	*get_next_line(int fd);
+char	*read_line(int fd, char *fd_box);
+int		find_end_line(char *s);
+char	*ft_strjoin(char *s1, char *s2);
+size_t	ft_strlen(const char *str);
+int		check_line(char *s);
+char	*get_line(char *str);
+char	*clear_box(char	*str);
+
+#endif
---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include "get_next_line.h"
+
+int main()
+{
+	int	fd1;
+	int fd2;
+	int i = 30;
+	char	*line1;
+	char	*line2;
+
+	fd1 = open("text1.txt", O_RDONLY);
+	fd2 = open("text2.txt", O_RDONLY);
+
+	while(i--)
+	{
+		line1 = get_next_line(fd1);
+		printf("%s", line1);
+		line2 = get_next_line(fd2);
+		printf("%s", line2);
+	}
+	close(fd1);
+	close(fd2);
+}
